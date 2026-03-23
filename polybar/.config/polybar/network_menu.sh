8254a07830d6c57ffc124b16a477ee6235de5e10
Abraham Steenhoek <ajsteenhoek@gmail.com>
2026-03-22 21:37:17 -0500

    Change network manager to use non-sudo commands (Requires adding network to user access)

--- polybar/.config/polybar/network_menu.sh
+++ polybar/.config/polybar/network_menu.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
 
-wifi_interface=$(ip link | grep -oE "wlp[0-9s]+")
+# Get the Wi-Fi interface
+wifi_interface=$(ls /sys/class/net | grep -E "^wlp" | head -n 1)
+
+# Function to run wpa_cli (NO SUDO NEEDED NOW)
+run_wpa() {
+    /usr/sbin/wpa_cli -i "$wifi_interface" "$@"
+}
 
 # Scan for networks
-/usr/sbin/wpa_cli -i $wifi_interface scan > /dev/null
+run_wpa scan > /dev/null
 sleep 2
 
-# Get scan results
-scan_results=$(/usr/sbin/wpa_cli -i $wifi_interface scan_results | awk 'NR>2 {print $NF}')
+# Get scan results and format them
+scan_results=$(run_wpa scan_results | awk 'NR>2 {
+    ssid=""; for(i=5; i<=NF; i++) ssid=(ssid $i " ");
+    sub(/ $/, "", ssid);
+    if (ssid != "") print ssid " (" $3 " dBm)"
+}' | sort -u)
+
+if [ -z "$scan_results" ]; then
+    dunstify -u critical "Network" "No networks found. Ensure Wi-Fi is on."
+    exit 1
+fi
 
 # Choose SSID using rofi
-chosen_ssid=$(echo -e "$scan_results" | rofi -dmenu -i -p "Select Wifi" -theme-str 'window { width: 30%; } listview { lines: 10; }')
+chosen_line=$(echo -e "$scan_results" | rofi -dmenu -i -p "Select Wifi" -theme-str 'window { width: 40%; } listview { lines: 15; }')
 
-if [ -n "$chosen_ssid" ]; then
+if [ -n "$chosen_line" ]; then
+    chosen_ssid=$(echo "$chosen_line" | sed 's/ (.* dBm)$//')
+    
     # Get password using rofi
     password=$(rofi -dmenu -password -p "Password for $chosen_ssid" -theme-str 'window { width: 30%; } listview { lines: 0; }')
     
     if [ -n "$password" ]; then
-        # Connect using wpa_cli
-        net_id=$(/usr/sbin/wpa_cli -i $wifi_interface add_network | tail -n 1)
-        /usr/sbin/wpa_cli -i $wifi_interface set_network $net_id ssid "\"$chosen_ssid\""
-        /usr/sbin/wpa_cli -i $wifi_interface set_network $net_id psk "\"$password\""
-        /usr/sbin/wpa_cli -i $wifi_interface enable_network $net_id
-        /usr/sbin/wpa_cli -i $wifi_interface save_config
+        # Add network and set credentials
+        net_id=$(run_wpa add_network | tail -n 1)
+        run_wpa set_network "$net_id" ssid "\"$chosen_ssid\""
+        run_wpa set_network "$net_id" psk "\"$password\""
+        run_wpa enable_network "$net_id"
+        run_wpa select_network "$net_id"
+        run_wpa save_config
         
-        dunstify -a "Network" "Connecting to $chosen_ssid..." -t 3000
+        dunstify -a "Network" "Connecting to $chosen_ssid..." -t 5000
     fi
 fi
